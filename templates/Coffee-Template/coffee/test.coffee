bf079ac1d4ef1853551950bc217ada1833196f98
milad <milad@miladnazeri.com>
2018-03-05 10:40:36 -0800

    added templates

---
+++ templates/Coffee-Template/coffee/test.coffee
@@ -0,0 +1,9 @@
+number = 12
+opposite = true
+
+number = -42 if opposite
+
+add = (a,b) ->
+    c = a+b
+    console.log "Sum of the two numbers is: "+c
+add 10,20
---
+++ templates/Coffee-Template/dist/test.js
@@ -0,0 +1,23 @@
+"use strict";
+
+(function () {
+  var add, number, opposite;
+
+  number = 12;
+
+  opposite = true;
+
+  if (opposite) {
+    number = -42;
+  }
+
+  add = function add(a, b) {
+    var c;
+    c = a + b;
+    return console.log("Sum of the two numbers is: " + c);
+  };
+
+  add(10, 20);
+}).call(undefined);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC5qcyIsInNvdXJjZVJvb3QiOiIuLiIsInNvdXJjZXMiOlsiY29mZmVlXFx0ZXN0LmNvZmZlZSJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLGFBQUE7TUFBQSxLQUFBLFFBQUE7O0FBQUEsV0FBUzs7QUFDVCxhQUFXOztBQUVYLE1BQWdCLEFBQWhCO0FBQUEsYUFBUyxDQUFDLEFBQVY7OztBQUVBLFFBQU0sYUFBQyxBQUFELEdBQUcsQUFBSCxHQUNGO1FBQUE7QUFBQSxRQUFJLElBQUU7V0FDTixBQUFPLFFBQUMsQUFBUixJQUFZLGdDQUE4QixBQUExQyxBQUZFOzs7QUFHTixNQUFJLEFBQUosSUFBTyxBQUFQLEFBUkEiLCJzb3VyY2VzQ29udGVudCI6WyJudW1iZXIgPSAxMlxyXG5vcHBvc2l0ZSA9IHRydWVcclxuXHJcbm51bWJlciA9IC00MiBpZiBvcHBvc2l0ZVxyXG5cclxuYWRkID0gKGEsYikgLT5cclxuICAgIGMgPSBhK2JcclxuICAgIGNvbnNvbGUubG9nIFwiU3VtIG9mIHRoZSB0d28gbnVtYmVycyBpczogXCIrY1xyXG5hZGQgMTAsMjBcclxuIl19
+//# sourceURL=D:\_ROLC\_ROLC\Create\C_Programming\C_Node\Coffee-Template\coffee\test.coffee
